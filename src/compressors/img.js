239e5f7a99d6398665d13424b08b66e7c3c48cae
Gregory Schier <gschier1990@gmail.com>
2019-05-31 19:46:15 -0400

    Fix image minifications args

--- src/compressors/img.js
+++ src/compressors/img.js
@@ -7,18 +7,18 @@ var imageminPngquant = require('imagemin-pngquant');
 module.exports.fileType = 'Image';
 
 module.exports.process = function (buffer, fullPath, callback) {
-    imagemin.buffer(buffer, {
-        plugins: [
-            imageminGifsicle(),
-            imageminSvgo(),
-            imageminMozjpeg({}),
-            imageminPngquant({quality: '65-80'})
-        ]
-    }).then(function (data) {
-        callback(null, data);
-    }, function (err) {
-        console.warn('Failed to compress image. Leaving as is:', err);
-        callback(null, buffer);
-    });
+  imagemin.buffer(buffer, {
+    plugins: [
+      imageminGifsicle(),
+      imageminSvgo(),
+      imageminMozjpeg({}),
+      imageminPngquant({ quality: [65, 80] }),
+    ],
+  }).then(function (data) {
+    callback(null, data);
+  }, function (err) {
+    console.log('Failed to compress image. Leaving as is:', err);
+    callback(null, buffer);
+  });
 };
 
